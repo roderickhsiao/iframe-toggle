ebc8d5e7c4aac34aae1013c0967bce6735fab0ae
Roderick Hsiao <roderickhsiao@gmail.com>
2022-04-30 19:58:16 -0700

    Enahnace prototype

--- components/MainApp.tsx
+++ components/MainApp.tsx
@@ -2,13 +2,11 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 
-import styles from '../styles/Home.module.css';
-
 const Home = ({ title }: { title: string }) => {
   return (
-    <div className={styles.container}>
+    <div className="min-h-screen isolated">
       <Head>
-        <title>Main App</title>
+        <title>{title}</title>
       </Head>
 
       <header className="sticky w-full top-0 border-slate-200 border-b ">
@@ -17,42 +15,52 @@ const Home = ({ title }: { title: string }) => {
         </div>
       </header>
 
-      <main className={styles.main}>
+      <main className="antialiased text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900 py-12">
         <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
           {title}
         </h1>
 
-        <p className={styles.description}>
+        <p className="my-6 max-w-3xl mx-auto text-lg text-center">
           Experiments different solutions handling widget transitions.
         </p>
 
-        <div className={styles.grid}>
-          <Link href="/iframe-composition">
-            <a className={styles.card}>
-              <h2>Single Iframe Composition</h2>
-              <p>
-                Use Single Iframe/Single PWA to handle state change, use
-                SwitchTransition
-              </p>
-            </a>
-          </Link>
-
-          <Link href="/iframe-composition-switch">
-            <a className={styles.card}>
-              <h2>Single Iframe Switch Layout</h2>
-              <p>Toggle two layout using CSSTransition</p>
-            </a>
-          </Link>
-
-          <Link href="/iframe-composition-multiple">
-            <a className={styles.card}>
-              <h2>Multiple Iframe</h2>
-              <p>
-                Use Multiple Iframe/Multiple PWA controlled by centralized js
-              </p>
-            </a>
-          </Link>
-        </div>
+        <ul className="bg-slate-50 p-4 lg:max-w-3xl mx-auto sm:px-8 sm:pt-6 sm:pb-8 lg:p-4 xl:px-8 xl:pt-6 xl:pb-8 grid sm:grid-cols-2 xl:grid-cols-2 gap-4 text-sm leading-6 dark:bg-slate-900/40 dark:ring-1 dark:ring-white/5">
+          <li className="group cursor-pointer rounded-md p-3 bg-white ring-1 ring-slate-200 shadow-sm hover:bg-blue-500 hover:ring-blue-500 hover:shadow-md dark:bg-slate-700 dark:ring-0 dark:highlight-white/10 dark:hover:bg-blue-500">
+            <Link href="/iframe-composition">
+              <a className="grid sm:block lg:grid xl:block grid-cols-2 grid-rows-2 items-center">
+                <h2 className="font-semibold text-slate-900 group-hover:text-white dark:text-slate-100">
+                  Single Iframe Composition
+                </h2>
+                <p className="group-hover:text-blue-200">
+                  Use Single Iframe/Single PWA to handle state change, use
+                  SwitchTransition
+                </p>
+              </a>
+            </Link>
+          </li>
+          <li className="group cursor-pointer rounded-md p-3 bg-white ring-1 ring-slate-200 shadow-sm hover:bg-blue-500 hover:ring-blue-500 hover:shadow-md dark:bg-slate-700 dark:ring-0 dark:highlight-white/10 dark:hover:bg-blue-500">
+            <Link href="/iframe-composition-switch">
+              <a className="grid sm:block lg:grid xl:block grid-cols-2 grid-rows-2 items-center">
+                <h2 className="font-semibold text-slate-900 group-hover:text-white dark:text-slate-100">
+                  Single Iframe Switch Layout
+                </h2>
+                <p className="group-hover:text-blue-200">Toggle two layout using CSSTransition</p>
+              </a>
+            </Link>
+          </li>
+          <li className="group cursor-pointer rounded-md p-3 bg-white ring-1 ring-slate-200 shadow-sm hover:bg-blue-500 hover:ring-blue-500 hover:shadow-md dark:bg-slate-700 dark:ring-0 dark:highlight-white/10 dark:hover:bg-blue-500">
+            <Link href="/iframe-composition-multiple">
+              <a className="grid sm:block lg:grid xl:block grid-cols-2 grid-rows-2 items-center">
+                <h2 className="font-semibold text-slate-900 group-hover:text-white dark:text-slate-100">
+                  Multiple Iframe
+                </h2>
+                <p className="group-hover:text-blue-200">
+                  Use Multiple Iframe/Multiple PWA controlled by centralized js
+                </p>
+              </a>
+            </Link>
+          </li>
+        </ul>
       </main>
     </div>
   );
